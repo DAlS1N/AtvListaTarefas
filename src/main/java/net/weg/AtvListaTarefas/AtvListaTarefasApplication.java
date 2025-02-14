debbb47023a9b06baf918a5b187d3a0c0234b7e6
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2025-02-14 20:26:58 -0300

    Initial commit

---
+++ src/main/java/net/weg/AtvListaTarefas/AtvListaTarefasApplication.java
@@ -0,0 +1,13 @@
+package net.weg.AtvListaTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvListaTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvListaTarefasApplication.class, args);
+	}
+
+}
